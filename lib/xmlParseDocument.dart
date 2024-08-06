44b906186e7a2128644c713f1623079ada9bf335
Stefan Ronnkvist <118016526+StefanRonnkvist@users.noreply.github.com>
2024-08-06 16:09:22 -0500

    Updated Display

--- lib/xmlParseDocument.dart
+++ lib/xmlParseDocument.dart
@@ -2,13 +2,21 @@ import 'package:flutter/services.dart';
 import 'package:xml/xml.dart' as xml;
 
 Future<List<String>> xmlParseDocument() async {
-  String newDocument = await rootBundle.loadString('MachineData1.xml');
-  xml.XmlDocument document = xml.XmlDocument.parse(newDocument);
-  String rootElement = document.rootElement.name.toString();
-  xml.XmlElement rootNode = document.findElements(rootElement).first;
-  Iterable<xml.XmlElement> returnXmlElements = _returnXmlElements(rootNode);
   List<String> returnList = [];
-  _returnXmlAttributes(returnXmlElements, returnList);
+  String newDocument = await rootBundle.loadString('president.xml');
+  xml.XmlDocument document = xml.XmlDocument.parse(newDocument);
+  if (document.declaration?.encoding != null) {
+    String string1 = document.declaration!.encoding.toString();
+    returnList.add("encoding : $string1");
+  }
+  if (document.declaration?.version != null) {
+    String string1 = document.declaration!.version.toString();
+    returnList.add("version : $string1");
+  }
+  String rootElement = document.rootElement.name.toString();
+  xml.XmlElement rootNodes = document.findElements(rootElement).first;
+  Iterable<xml.XmlElement> returnXmlElements = _returnXmlElements(rootNodes);
+  _returnXmlElementsIterable(returnXmlElements, returnList);
   return returnList;
 }
 
@@ -16,20 +24,26 @@ Iterable<xml.XmlElement> _returnXmlElements(xml.XmlElement testValue) {
   return testValue.childElements;
 }
 
-void _returnXmlAttributes(
+void _returnXmlElementsIterable(
     Iterable<xml.XmlElement> testValues, List returnList) {
+  String longString = "";
   for (xml.XmlElement testValue in testValues) {
-    List<xml.XmlAttribute> testValueAttributes = testValue.attributes;
-    for (int j = 0; testValueAttributes.length > j; j++) {
-      String testValueName = testValue.name.toString();
-      String testValueList = testValueAttributes[j].toString();
-      returnList.add("$testValueName $testValueList");
-      if (j == testValueAttributes.length - 1 &&
-          testValueAttributes.length > 1) {
-        String newString = testValue.innerXml;
-        returnList.add("Label $newString");
+    if (testValue.children.length > 1) {
+      _returnXmlElementsIterable(_returnXmlElements(testValue), returnList);
+    } else {
+      String string1 = testValue.name.toString();
+      String string2 = testValue.innerText;
+      longString += "$string1 : $string2, ";
+      //returnList.add("$string1 : $string2");
+    }
+    if (testValue.attributes.isNotEmpty) {
+      for (xml.XmlAttribute testValueAttributes in testValue.attributes) {
+        String string1 = testValueAttributes.name.toString();
+        String string2 = testValueAttributes.value.toString();
+        longString += "$string1 : $string2, ";
+        //returnList.add("$string1 : $string2");
       }
     }
-    _returnXmlAttributes(testValue.childElements, returnList);
   }
+  returnList.add(longString);
 }
