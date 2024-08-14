520d0f5f28e0d04fbe4163df8f628479e4b87b90
Stefan Ronnkvist <118016526+StefanRonnkvist@users.noreply.github.com>
2024-08-14 18:02:22 -0500

    Updated Code for Recursive

--- lib/xmlParseDocument.dart
+++ lib/xmlParseDocument.dart
@@ -3,6 +3,7 @@ import 'package:xml/xml.dart' as xml;
 
 Future<List<String>> xmlParseDocument() async {
   List<String> returnList = [];
+  List<xml.XmlElement> elementLists = [];
   String newDocument = await rootBundle.loadString('president.xml');
   xml.XmlDocument document = xml.XmlDocument.parse(newDocument);
   if (document.declaration?.encoding != null) {
@@ -15,35 +16,39 @@ Future<List<String>> xmlParseDocument() async {
   }
   String rootElement = document.rootElement.name.toString();
   xml.XmlElement rootNodes = document.findElements(rootElement).first;
-  Iterable<xml.XmlElement> returnXmlElements = _returnXmlElements(rootNodes);
-  _returnXmlElementsIterable(returnXmlElements, returnList);
+  returnList.add("Root Element : $rootElement");
+  findLastElement(rootNodes, elementLists);
+  for (xml.XmlElement node in elementLists) {
+    Set<String> parentNodeSet = {};
+    parentNodeSet.add(node.toString());
+    parentNodeSet.addAll(findElementParent(node, rootElement, parentNodeSet));
+    List<String> parentNodeList = parentNodeSet.toList();
+    Iterable<String> newParentNodeList=parentNodeList.reversed;
+    returnList.add(newParentNodeList.toString());
+    parentNodeList.clear();
+    parentNodeSet.clear();
+  }
   return returnList;
 }
 
-Iterable<xml.XmlElement> _returnXmlElements(xml.XmlElement testValue) {
-  return testValue.childElements;
-}
-
-void _returnXmlElementsIterable(
-    Iterable<xml.XmlElement> testValues, List returnList) {
-  String longString = "";
-  for (xml.XmlElement testValue in testValues) {
-    if (testValue.children.length > 1) {
-      _returnXmlElementsIterable(_returnXmlElements(testValue), returnList);
+void findLastElement(xml.XmlElement nodes, List<xml.XmlElement> elementList) {
+  for (xml.XmlElement childNode in nodes.childElements) {
+    if (childNode.childElements.isNotEmpty) {
+      (findLastElement(childNode, elementList));
     } else {
-      String string1 = testValue.name.toString();
-      String string2 = testValue.innerText;
-      longString += "$string1 : $string2, ";
-      //returnList.add("$string1 : $string2");
-    }
-    if (testValue.attributes.isNotEmpty) {
-      for (xml.XmlAttribute testValueAttributes in testValue.attributes) {
-        String string1 = testValueAttributes.name.toString();
-        String string2 = testValueAttributes.value.toString();
-        longString += "$string1 : $string2, ";
-        //returnList.add("$string1 : $string2");
-      }
+      elementList.add(childNode);
     }
   }
-  returnList.add(longString);
+}
+
+Set<String> findElementParent(
+    xml.XmlElement nodes, String rootElement, Set<String> parentNodeList) {
+  if (nodes.hasParent) {
+    xml.XmlElement parentNode = nodes.parentElement!;
+    parentNodeList.add("${parentNode.name} ${parentNode.attributes}");
+    if (rootElement != parentNode.name.toString()) {
+      findElementParent(nodes.parentElement!, rootElement, parentNodeList);
+    }
+  }
+  return (parentNodeList);
 }
